8696309f474dc343145ce443d65ca5333ecac6fe
Jatin Shamsher Singh Jasrotia <131923664+Jatin-35@users.noreply.github.com>
2025-07-24 21:05:40 +0530

    Time: 1 ms (99.53%), Space: 57.4 MB (13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        
 
         int n = nums.length;
-        int maxSum = nums[0];
+
+        int max = nums[0];
         int maxEnding = nums[0];
-        
-        for(int i = 1 ; i < n ; i++){
+
+
+        for(int i = 1; i < n ; i++){
 
             maxEnding = Math.max(nums[i] , maxEnding + nums[i]);
-            maxSum = Math.max(maxSum ,maxEnding);  
+            max = Math.max(max , maxEnding);
         }
 
-        return maxSum;
-       
+        return max;
     }
 }
