f8407230da3cf59fc603acb0161711172889ae46
Jatin Shamsher Singh Jasrotia <131923664+Jatin-35@users.noreply.github.com>
2025-08-10 14:00:29 +0530

    Time: 0 ms (100%), Space: 41.1 MB (25.91%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -2,14 +2,9 @@ class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n <= 0) return false; // ✅ Changed from n == 0 to n <= 0
         
-        String s = Integer.toBinaryString(n);
-        int count = 0;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '1'){
-                count++;
-            }
-        }
+        if((n & n-1) == 0) return true;
+
+        return false;
         
-        return count == 1; // ✅ Simplified condition
     }
 }
