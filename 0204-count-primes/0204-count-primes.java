7aca116d69ceac851d0da5799a5d627d90f71920
Jatin Shamsher Singh Jasrotia <131923664+Jatin-35@users.noreply.github.com>
2025-08-16 17:07:08 +0530

    Time: 92 ms (87.51%), Space: 49.4 MB (48.15%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,22 +1,25 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n < 3) return 0; // only prime < 3 is 2
-
-        boolean[] isPrime = new boolean[n];
-        Arrays.fill(isPrime, true);
+        
+        if(n < 2) return 0;
+        boolean [] isPrime = new boolean[n];
+        Arrays.fill(isPrime , true);
         isPrime[0] = isPrime[1] = false;
+        
 
-        int count = n / 2; // assume half numbers are prime (all odds), adjust later
+        for(int i = 2 ; i * i < n ; i++){
 
-        for (int i = 3; i * i < n; i += 2) { // skip evens
-            if (isPrime[i]) {
-                for (int j = i * i; j < n; j += 2 * i) { // skip even multiples
-                    if (isPrime[j]) {
-                        isPrime[j] = false;
-                        count--;
-                    }
+            if(isPrime[i]){
+                for(int  j = i * i ; j < n ; j+=i){
+                    isPrime[j] = false;
                 }
             }
+        }
+
+        int count = 0;
+
+        for(boolean prime : isPrime){
+            if(prime) count++;
         }
 
         return count;
