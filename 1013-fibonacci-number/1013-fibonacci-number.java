76d3034500335bad1315c7c8036f3ee60a42c923
Jatin Shamsher Singh Jasrotia <131923664+Jatin-35@users.noreply.github.com>
2025-08-28 01:11:39 +0530

    Time: 40 ms (10.22%), Space: 44.4 MB (6.63%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,17 @@
 class Solution {
     public int fib(int n) {
         
+        int [] dp = new int[n+1];
+        Arrays.fill(dp , -1);
+
+        return helper(n , dp );
+
+    }
+
+    public int helper(int n , int [] dp){
         if(n == 1 || n == 0){
             return n;
         }
-
-        int [] dp = new int[n+1];
-        Arrays.fill(dp , -1);
         
         if(dp[n] != -1) return dp[n];
 
