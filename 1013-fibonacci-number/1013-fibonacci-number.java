9e5a18025da1c2deea3b4bde739b6040101a6bbb
Jatin Shamsher Singh Jasrotia <131923664+Jatin-35@users.noreply.github.com>
2025-08-28 01:46:48 +0530

    Time: 0 ms (100%), Space: 40.5 MB (49.41%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -6,13 +6,14 @@ class Solution {
 
         int prev1 = 1;
         int prev2 = 0;
+        int curr =0;
 
         for(int i = 2; i <= n; i++){
-            int curr = prev1 + prev2;
+            curr = prev1 + prev2;
             prev2 = prev1;
             prev1 = curr;
         }
 
-        return prev1;
+        return curr;
     }
 } 
