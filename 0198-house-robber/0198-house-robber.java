cf864186c4ddd61f377624f5a9f382a93de0d20a
Jatin Shamsher Singh Jasrotia <131923664+Jatin-35@users.noreply.github.com>
2025-09-07 03:07:34 +0530

    Time: 0 ms (100%), Space: 41.2 MB (46.76%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-
         int n = nums.length;
         if (n == 1) return nums[0];
-        int prev1 = nums[0];
-        int prev2 = 0;
 
-        for(int i = 1; i < n ; i++){
+        int prev1 = nums[0]; // best till house i-1
+        int prev2 = 0;       // best till house i-2
 
-            
-            int pick = nums[i];
-            if (i > 1) pick += prev2;
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i] + (i > 1 ? prev2 : 0); // take current + dp[i-2]
+            int notPick = prev1;                      // skip current, take dp[i-1]
 
-            int notPick = prev1;
+            int curr = Math.max(pick, notPick);
 
-            int curr = Math.max(pick , notPick);
-
+            // shift forward
             prev2 = prev1;
             prev1 = curr;
-            
         }
-        
 
         return prev1;
     }
